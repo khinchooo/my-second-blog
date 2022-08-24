7a5efd3a08429779768d5a41e88c3074e12e1a74
khinchooo <khinchooo@mm.cybermissions.co.jp>
2022-08-24 18:16:19 +0900

    draft and delete 機能を追加

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-@login_required
 def post_list(request):
   # 公開したプログ記事
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -45,3 +44,19 @@ def post_edit(request, pk):
     form = PostForm(instance=post)
   return render(request, 'blog/post_edit.html', { 'form': form})
 
+def post_draft_list(request):
+  # 末公開のブログ記事
+  posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+  return render(request, 'blog/post_draft_list.html', { 'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  post.publish()
+  return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  post.delete()
+  return redirect('post_list')
